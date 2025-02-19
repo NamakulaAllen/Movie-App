c8501ec64eb68c3215da7ecb32f455e8e937e15e
Namakula Allen <allenglain9@gmail.com>
2025-02-19 13:37:37 +0300

    assignment1

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'latest_movies.dart';  
+import 'package:cached_network_image/cached_network_image.dart';
+import '../services/movie_services.dart'; 
+import '../models/movie.dart';
+
 
 class HomeScreen extends StatelessWidget {
   final MovieService movieService = MovieService();
@@ -7,9 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Latest Movies'),
-      ),
+      appBar: AppBar(title: Text('Latest Movies')),
       body: FutureBuilder<List<Movie>>(
         future: movieService.fetchMovies(),
         builder: (context, snapshot) {
@@ -21,14 +22,63 @@ class HomeScreen extends StatelessWidget {
             return Center(child: Text('No movies found.'));
           } else {
             final movies = snapshot.data!;
-            return ListView.builder(
-              itemCount: movies.length,
-              itemBuilder: (context, index) {
-                final movie = movies[index];
-                return ListTile(
-                  title: Text(movie.title),
-                );
-              },
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Now Playing',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  // Horizontally Scrollable List of Movies
+                  SizedBox(
+                    height: 250, // Set a fixed height
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: movies.length,
+                      itemBuilder: (context, index) {
+                        final movie = movies[index];
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                          child: Column(
+                            children: [
+                              CachedNetworkImage(
+                                imageUrl: 'https://image.tmdb.org/t/p/w500${movie.posterPath}',
+                                imageBuilder: (context, imageProvider) => Container(
+                                  width: 140, // Increased width
+                                  height: 200, // Increased height
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                                placeholder: (context, url) => CircularProgressIndicator(),
+                                errorWidget: (context, url, error) => Icon(Icons.error),
+                              ),
+                              SizedBox(height: 8),
+                              SizedBox(
+                                width: 140,
+                                child: Text(
+                                  movie.title,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
             );
           }
         },
